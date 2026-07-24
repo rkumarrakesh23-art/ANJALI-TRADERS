b87c83c215126c05ab1efd580015498077db82f1
ANJALI TRADERS <rkumarrakesh23@gmail.com>
2026-07-24 20:06:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,20 @@ window.addEventListener("scroll", function(){
     }
 
 });
+// Back To Top Button
+let mybutton = document.getElementById("topBtn");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+};
+
+function topFunction() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
